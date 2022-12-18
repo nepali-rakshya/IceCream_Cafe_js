d403f22568278d5e9f79a6ffeb109c0d7a9ef6b9
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-19 00:40:19 +0545

    commit

--- index.js
+++ index.js
@@ -5,25 +5,77 @@ let stocks = {
   toppings: ["chocolate", "sprinkles"],
 };
 
-console.log(stocks[`${[2]}`]);
+let order = function (call_Production) {
+  alert(
+    "Welcome to our Ice-Cream Cafe : \nPlease select the fruits you want in your ice-cream from below : "
+  );
+  console.log("The available fruits are: \n");
+  let ice_fruits = stocks.fruits;
+  console.log(ice_fruits.toString());
+  console.log(" ");
 
-// let order = function (call_Production) {
-//   alert(
-//     "Welcome to our Ice-Cream Cafe : \nPlease select the fruits you want in your ice-cream from below : "
-//   );
-//   console.log("The available fruits are: \n");
+  let selectFruit = parseInt(
+    prompt("Which fruit do you want, write it's number: ")
+  );
+  let selectFruit_order = stocks.fruits[selectFruit - 1];
 
-//   let ice_fruits = stocks.fruits;
-//   console.log(ice_fruits.toString());
-//   console.log(" ");
-//   console.log("Please write the fruit you want in your icecream : ");
-//   let order_fruit = prompt("Fruits: ");
-//   let order_fruit_arr = order_fruit.split(",");
-//   console.log(order_fruit_arr);
+  setTimeout(() => {
+    console.log(selectFruit_order + " was selected");
+    console.log("");
+    call_Production();
+  }, 2000);
+};
 
-//   call_Production();
-// };
+let production = function () {
+  setTimeout(() => {
+    // First
+    console.log("Proudction has started");
 
-// let production = function (items) {};
+    setTimeout(() => {
+      // Second
+      console.log("The fruit has been choped");
 
-// order(production);
+      setTimeout(() => {
+        // Third
+        console.log(`Adding ${stocks.liquid[0]} and ${stocks.liquid[1]}`);
+
+        setTimeout(() => {
+          // Fourth
+          console.log("Start the machine");
+          console.log("");
+          let holder_arr = stocks.holder;
+          console.log(`The holders are ${holder_arr.toString()}`);
+
+          setTimeout(() => {
+            // Fifth
+            let holder = parseInt(
+              prompt("Please select the holder, in number from below: ")
+            );
+            let select_holder = stocks.holder[holder - 1];
+            console.log(`You have selected ${select_holder} `);
+            console.log("");
+            let toppings_arr = stocks.toppings;
+            console.log(`The toppings are ${toppings_arr.toString()}`);
+
+            setTimeout(() => {
+              // Sixth
+              let toppings = parseInt(
+                prompt("Please select the toppings, in number from below: ")
+              );
+              let select_toppings = stocks.toppings[toppings - 1];
+              console.log(`You have selected ${select_toppings} `);
+              console.log("");
+
+              setTimeout(() => {
+                // Seventh
+                alert("Thank you for visiting our cafe! ;P ");
+              }, 2000);
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
+    }, 2000);
+  }, 0);
+};
+
+order(production);
