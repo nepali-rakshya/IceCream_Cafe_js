7f61d46be41fa349e9edbdb66f5d8842f53df744
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-18 16:39:25 +0545

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "sprinkles"],
+};
+
+console.log(stocks[`${[2]}`]);
+
+// let order = function (call_Production) {
+//   alert(
+//     "Welcome to our Ice-Cream Cafe : \nPlease select the fruits you want in your ice-cream from below : "
+//   );
+//   console.log("The available fruits are: \n");
+
+//   let ice_fruits = stocks.fruits;
+//   console.log(ice_fruits.toString());
+//   console.log(" ");
+//   console.log("Please write the fruit you want in your icecream : ");
+//   let order_fruit = prompt("Fruits: ");
+//   let order_fruit_arr = order_fruit.split(",");
+//   console.log(order_fruit_arr);
+
+//   call_Production();
+// };
+
+// let production = function (items) {};
+
+// order(production);
