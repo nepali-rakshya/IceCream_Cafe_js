aee6dd71ea9a6a7f6f1c4d625be509afeb68ff0d
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-19 19:23:45 +0545

    commit

--- using_async_await.js
+++ using_async_await.js
@@ -5,14 +5,43 @@ let stocks = {
   toppings: ["chocolate", "sprinkles"],
 };
 
-async function order() {
-  try {
-    await abc;
-  } catch (error) {
-    console.log("This function doesn't exist", error);
-  } finally {
-    console.log("We are done");
-  }
+// function abc() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(console.log("I am happy"));
+//     }, 4000);
+//   });
+// }
+
+// async function order() {
+//   try {
+//     await abc();
+//   } catch (error) {
+//     console.log("This function doesn't exist", error);
+//   } finally {
+//     console.log("We are done");
+//   }
+// }
+
+// order();
+
+let timer = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log("We will be going on");
+      res(console.log("I am first"));
+      //   console.log("It's gonna be confusing");
+    }, 5000);
+  });
+};
+
+async function timerFlow(call_timer) {
+  console.log("I am first you know");
+  await timer();
+  console.log("Can I?");
+  console.log("Can You?");
+  call_timer();
+  console.log("Can We?");
 }
 
-order();
+timerFlow(timer);
