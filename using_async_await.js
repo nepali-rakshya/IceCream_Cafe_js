0ad535ce64c7449ddc9d692df43da9ea4d3852a4
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-19 18:35:49 +0545

    commit

---
+++ using_async_await.js
@@ -0,0 +1,18 @@
+let stocks = {
+  fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "sprinkles"],
+};
+
+async function order() {
+  try {
+    await abc;
+  } catch (error) {
+    console.log("This function doesn't exist", error);
+  } finally {
+    console.log("We are done");
+  }
+}
+
+order();
