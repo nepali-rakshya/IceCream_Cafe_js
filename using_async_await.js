ad80bc79f821b990c8808a1ea49a97f311531a61
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-20 17:42:31 +0545

    commit

--- using_async_await.js
+++ using_async_await.js
@@ -12,9 +12,37 @@ function shop(time) {
     if (is_shop_open) {
       setTimeout(res, time);
     } else {
-      rej(console.log("We are closed sir"));
+      // rej(console.log("We are closed sir"));
+      rej();
     }
   });
 }
 
-shop(2000);
+async function kitchen() {
+  try {
+    await shop(0000);
+    console.log("Hello Welcome to our Ice-Cream Cafe");
+    await shop(2000);
+    console.log(`${stocks.fruits[0]} was selected`);
+    await shop(0000);
+    console.log("Let's start the production");
+    await shop(2000);
+    console.log(`Cut the fruit.`);
+    await shop(1000);
+    console.log(`Add ${stocks.liquid[0]} and ${stocks.liquid[1]}`);
+    await shop(1000);
+    console.log(`Start the machine`);
+    await shop(2000);
+    console.log(`Selecting ${stocks.holder[0]} `);
+    await shop(3000);
+    console.log(`Selecting ${stocks.toppings[0]} as a topping`);
+    await shop(2000);
+    console.log(`Please enjoy your ice-cream`);
+  } catch (e) {
+    console.log("We are closed." + e);
+  } finally {
+    console.log("It was a great day everyone!");
+  }
+}
+
+kitchen();
